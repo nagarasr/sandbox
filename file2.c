b722487c9b1ecdba03ff7ad34839c36705126067
Nagaraju Sripathi <NAGARASR@GMAIL.COM>
2024-09-14 09:18:25 +0530

    New files added

---
+++ file2.c
@@ -0,0 +1,8 @@
+// This is function file commiting into github
+#include <stdio.h>
+
+int function2()
+{
+	printf ("This is function 2");
+	return 0;
+}
--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 // This is first file commiting into github
 #include <stdio.h>
 #include <file1.h>
+#include <file2.h>
 int main()
 {
 	printf ("This is new hello world");
 	function1();
+	function2();
 	return 0;
 }
